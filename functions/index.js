7308f475a4bfc7efebdbf788990a99eabac5f5a6
Jay Whitsitt <jay.whitsitt@gmail.com>
2023-12-29 17:53:09 -0600

    Add larger static values

--- functions/index.js
+++ functions/index.js
@@ -9,17 +9,139 @@ exports.helloWorld = onRequest((request, response) => {
 exports.create = onCall((request) => {
     // Placeholder to return static values
     return {
-        groups: [
-            {
-                color: 'orange',
-                hex: '#FF9600',
-                properties: [
-                    {
-                        name: 'Santa\'s Workshop',
-                        rent: 450,
-                    },
-                ],
-            },
-        ],
+     "groups": [
+       {
+         "color": "brown",
+         "hex": "#964B00",
+         "properties": [
+           {
+             "name": "Santa's Workshop",
+             "rent": 450
+           },
+           {
+             "name": "North Pole Playground",
+             "rent": 550
+           }
+         ]
+       },
+       {
+         "color": "railroad",
+         "hex": "#646464",  // Gray for railroads
+         "properties": [
+           {
+             "name": "Polar Express",
+             "rent": 250
+           },
+           {
+             "name": "Candy Cane Express",
+             "rent": 250
+           }
+         ]
+       },
+       {
+         "color": "purple",
+         "hex": "#963296",
+         "properties": [
+           {
+             "name": "Gingerbread Village",
+             "rent": 300
+           },
+           {
+             "name": "Reindeer Games Arena",
+             "rent": 350
+           },
+           {
+             "name": "Caroling Competition Stage",
+             "rent": 400
+           }
+         ]
+       },
+       {
+         "color": "blue",
+         "hex": "#0064C8",
+         "properties": [
+           {
+             "name": "Frozen Lake",
+             "rent": 280
+           },
+           {
+             "name": "Sledding Hill",
+             "rent": 330
+           },
+           {
+             "name": "Snow Globe Factory",
+             "rent": 380
+           }
+         ]
+       },
+       {
+         "color": "green",
+         "hex": "#009600",
+         "properties": [
+           {
+             "name": "Christmas Tree Farm",
+             "rent": 260
+           },
+           {
+             "name": "Elf Toy Shop",
+             "rent": 310
+           },
+           {
+             "name": "Christmas Cookie Bakery",
+             "rent": 360
+           }
+         ]
+       },
+       {
+         "color": "orange",
+         "hex": "#FF9600",
+         "properties": [
+           {
+             "name": "Department Store Wonderland",
+             "rent": 400
+           },
+           {
+             "name": "Cozy Toy Shop",
+             "rent": 450
+           },
+           {
+             "name": "Santa's Sleighport",
+             "rent": 500
+           }
+         ]
+       },
+       {
+         "color": "yellow",
+         "hex": "#FFFF00",
+         "properties": [
+           {
+             "name": "Mrs. Claus' Kitchen",
+             "rent": 350
+           },
+           {
+             "name": "The Naughty and Nice List",
+             "rent": 400
+           },
+           {
+             "name": "North Pole Post Office",
+             "rent": 450
+           }
+         ]
+       },
+       {
+         "color": "utility",
+         "hex": "#646464",  // Gray for utilities
+         "properties": [
+           {
+             "name": "Snowy Owl Post",
+             "rent": 150
+           },
+           {
+             "name": "Abominable Snowman Tour",
+             "rent": 150
+           }
+         ]
+       }
+     ]
     };
 });
