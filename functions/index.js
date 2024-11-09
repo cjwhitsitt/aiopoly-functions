2cff5e29108fd84c475dc62f89d480e5994949a7
jay.whitsitt <jay.whitsitt@gmail.com>
2024-11-09 02:13:51 +0000

    Add comments and files for IDX

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const {onRequest, onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const {VertexAI} = require('@google-cloud/vertexai');
 
+// Use `firebase emulators:start` from root to run locally
+
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
@@ -16,8 +18,36 @@ exports.create = onCall(async (request) => {
 
   // Initialize Vertex with your Cloud project and location
   const vertex_ai = new VertexAI({project: 'aiopoly', location: 'us-central1'});
+  
   // Available models: https://cloud.google.com/vertex-ai/docs/generative-ai/learn/models
   const model = 'gemini-1.5-flash';
+
+  // Define the schema for the response
+  // const jsonSchema = {
+  //   type: 'object',
+  //   properties: {
+  //     groups: {
+  //       type: 'array',
+  //       items: {
+  //         type: 'object',
+  //         properties: {
+  //           color: { type: 'string' },
+  //           hex: { type: 'string' },
+  //           properties: {
+  //             type: 'array',
+  //             items: {
+  //               type: 'object',
+  //               properties: {
+  //                 name: { type: 'string' },
+  //                 rent: { type: 'number' },
+  //               },
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
 
   // Instantiate the models
   const generativeModel = vertex_ai.preview.getGenerativeModel({
@@ -26,7 +56,9 @@ exports.create = onCall(async (request) => {
     generation_config: {
       "max_output_tokens": 2048,
       "temperature": 0.9,
-      "top_p": 1
+      "top_p": 1,
+      // responseMimeType: "application/json",
+      // responseSchema: jsonSchema,
     },
   });
 
@@ -49,6 +81,8 @@ exports.create = onCall(async (request) => {
 }
 `;
 
+  // const prompt = `Provide Monopoly board spaces for a game themed around ${theme}. Don't include non-purchasable spaces like Go or Community Chest. The hex property should be in hexidecimal.`;
+
   const req = {
     contents: [{role: 'user', parts: [{text: prompt}]}],
   };
@@ -56,6 +90,15 @@ exports.create = onCall(async (request) => {
   const content = await generativeModel.generateContent(req);
   const string = content.response.candidates.at(0).content.parts.at(0).text;
   const obj = JSON.parse(string);
-  console.log(obj);
+  console.log(JSON.stringify(obj, null, 2));
   return obj;
 });
+
+
+/*
+curl --request POST --url http://127.0.0.1:5001/aiopoly/us-central1/create --header 'content-type: application/json' --data '{
+  "data": {
+    "theme": "Alice in Wonderland"
+  }
+}'
+*/
